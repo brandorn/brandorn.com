065269154b36c212103027650bca739b9ce2d36a
Brandon Dorn <brandon.s.dorn@gmail.com>
2016-11-15 22:24:14 -0500

    Rearranges some quotes

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 // Landing quote carousel thing
 
 var quotes = [
+  { quote: "You mustn’t wish for another life. You mustn’t want to be somebody else. What you must do is this: ‘Rejoice evermore. Pray without ceasing. In everything give thanks.’ I am not all the way capable of so much, but those are the right instructions.", author: "Wendell Berry", source: "Hannah Coulter"},
   { quote: "In our daily experience words are used as a means to convey meaning. In the Bible to speak is to act, and the word is more than an instrument of expression; it is a vessel of divine power, the mystery of creation.", author: "Abraham Joshua Heschel", source: "God in Search of Man"},
   { quote: "That is the best of what the old heads meant when they spoke of being ‘politically conscious’–as much a series of actions as a state of being, a constant questioning, questioning as ritual, questioning as exploration rather than the search for certainty.", author: "Ta-Nehisi Coates", source: "Between the World and Me"},
   { quote: "I feel sometimes as if I were a child who opens its eyes on the world once and sees amazing things it will never know any names for and then has to close its eyes again.", author: "Marilynne Robinson", source: "Gilead"},
   { quote: "Ask questions. Find out what has been going on all these years before you were born.", author: "Deuteronomy 4.32", source: "The Message"},
-  { quote: "You mustn’t wish for another life. You mustn’t want to be somebody else. What you must do is this: ‘Rejoice evermore. Pray without ceasing. In everything give thanks.’ I am not all the way capable of so much, but those are the right instructions.", author: "Wendell Berry", source: "Hannah Coulter"},
-   { quote: "I cannot find language of sufficient energy to convey my sense of the sacredness of private integrity.", author: "Emerson", source: "Lecture on the Times"},
-   { quote: "Introverts are collectors of thoughts, and solitude is where the collection is curated and rearranged to make sense of the present and future.", author: "Laurie Helgoe", source: "Revenge of the Introverts"}
+  { quote: "I cannot find language of sufficient energy to convey my sense of the sacredness of private integrity.", author: "Emerson", source: "Lecture on the Times"},
+  { quote: "Introverts are collectors of thoughts, and solitude is where the collection is curated and rearranged to make sense of the present and future.", author: "Laurie Helgoe", source: "Revenge of the Introverts"}
 
 ],  i = 0,
     blockquote = document.getElementById('quote'),
